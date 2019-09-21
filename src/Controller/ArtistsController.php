20ef4004f1e65ecaeef034b2f7a359503e29bf8e
skagzilla <skaggskd88@gmail.com>
2019-09-21 13:26:16 -0500

    so messy, but displaying list of top artists

--- src/Controller/ArtistsController.php
+++ src/Controller/ArtistsController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use SpotifyWebAPI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,8 +10,31 @@ class ArtistsController extends AbstractController
   /**
    * @Route("/", name="home")
    */
-  public function displayList()
+  public function displayList(Request $request)
   {
-    return $this->render('artistList.html.twig');
+    $id = $_ENV['SPOTIFY_CLIENT_ID'];
+    $secret = $_ENV['SPOTIFY_CLIENT_SECRET'];
+
+    $session = new SpotifyWebAPI\Session(
+        $id,
+        $secret,
+        'http://localhost:8000'
+      );
+
+    if ($request->query->has('code')) {
+      $code = $request->query->get("code");
+      $api = new SpotifyWebAPI\SpotifyWebAPI();
+
+      $session->requestAccessToken($code);
+      $api->setAccessToken($session->getAccessToken());
+      $artists = $api->getMyTop('artists')->items;
+
+      return $this->render('artistList.html.twig', [ 'artists' => $artists ] );
+    } else {
+      $url = $session->getAuthorizeUrl([
+        'scope' => [ 'user-top-read' ]
+      ]);
+      return $this->redirect($url);
+    }
   }
 }
