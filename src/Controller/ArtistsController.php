a10e861d45b7ab4671ca3b4c60bce3e4aae47972
skagzilla <skaggskd88@gmail.com>
2019-09-25 22:59:34 -0500

    starting work on artist details page

--- src/Controller/ArtistsController.php
+++ src/Controller/ArtistsController.php
@@ -14,17 +14,28 @@ class ArtistsController extends AbstractController
    */
   public function home(Request $request, SessionInterface $session)
   {
-    // TODO: move logic into new
+    // TODO: move logic into new service
     if ($request->hasPreviousSession()) {
       if ($session->has('accessToken')) {
         $api = new SpotifyWebAPI\SpotifyWebAPI();
         $api->setAccessToken($session->get('accessToken'));  // TODO: check to see if the token needs to be refreshed
         $artists = $api->getMyTop('artists')->items;
-
-        return $this->render('artistList.html.twig', [ 'artists' => $artists ] );
+        
+        return $this->render('artistList.html.twig', [ 'artists' => $artists ]);
       }
     } else {
       return $this->render('welcome.html.twig' );
     }
+  }
+
+  /**
+   * @Route("/artist/{artistId}", name="artistDetails")
+   */
+  public function artistDetails(Request $request, string $artistId, SessionInterface $session) {
+    $api = new SpotifyWebAPI\SpotifyWebAPI();
+    $api->setAccessToken($session->get('accessToken'));
+    $tracks = $api->getArtistTopTracks($artistId, [ "country" => "US" ])->tracks;
+    var_dump($tracks[0]->album->artists[0]); // TODO: show the artists image in the background of the page
+    return $this->render('artistDetails.html.twig', [ 'tracks' => $tracks ]);
   }
 }
